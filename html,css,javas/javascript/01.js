57a97d56b68a4a1ea1bda23ff55de3f393c8c115
Gustavo <gustavod.oliveira@outlook.com>
2023-05-23 15:30:50 -0300

    Initial commit

---
+++ html,css,javas/javascript/01.js
@@ -0,0 +1,20 @@
+function lifeInWeeks(age) {
+    
+/************Don't change the code above************/    
+    
+    //Write your code here.
+    var maxAge = 90
+    var days = (maxAge - age) *365
+    var week = (maxAge - age) *52
+    var month = (maxAge - age) *12
+    alert(`Voce tem ${days} dias, ${week} semanas ${month} meses ate voce completar 90 anos`)
+
+
+    
+    
+    
+    
+/*************Don't change the code below**********/
+}
+
+lifeInWeeks(18)
---
+++ html,css,javas/mudar cor/script.js
@@ -0,0 +1,10 @@
+document.querySelector(".bmn").addEventListener('click', cor)
+
+function cor (){
+       document.querySelector('body').setAttribute ('class', 'black')
+}
+
+document.querySelector('.btn').addEventListener('click', rem)
+function rem () {
+       document.querySelector('body').setAttribute('class', 'light')
+}
