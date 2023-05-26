d8bb592adb41a81890629ec29c0743834cae873f
Gustavo <gustavod.oliveira@outlook.com>
2023-05-25 21:06:19 -0300

    ultimas praticas

--- javascript/drum chalenger.05/Drum Kit Starting Files/index.js
+++ javascript/drum chalenger.05/Drum Kit Starting Files/index.js
@@ -2,9 +2,23 @@ var numDrum = document.querySelectorAll(".drum").length ;
 
 for (var i = 0 ; i<numDrum; i++) {
 
-    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
-        
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+
+        alert('helo')
+
+     var buttonInnerHtml = numDrum.innerHtml ;
+     
+     switch (buttonInnerHtml) {
+        case 'w':
+            var tom1 = new Audio('sounds/tom-1.mp3') ;
+            tom1.play();
+            
+            break;
+     
+        default:
+            break;
+     }
 
     })
-}
-
+   
+}     
