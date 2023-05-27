1650b0259b36c59d018df3cb9a0c7a7f23cca1e8
Gustavo <gustavod.oliveira@outlook.com>
2023-05-27 15:20:30 -0300

    att

--- javascript/drum chalenger.05/Drum Kit Starting Files/index.js
+++ javascript/drum chalenger.05/Drum Kit Starting Files/index.js
@@ -4,21 +4,79 @@ for (var i = 0 ; i<numDrum; i++) {
 
     document.querySelectorAll(".drum")[i].addEventListener("click", function (){
 
-        alert('helo')
 
-     var buttonInnerHtml = numDrum.innerHtml ;
-     
-     switch (buttonInnerHtml) {
+     var buttonInnerHtml = this.innerHTML;
+     makesound(buttonInnerHtml);
+    
+     })
+
+    };   
+
+document.addEventListener('keypress', function(event) {
+
+    makesound(event.key);
+    animation(event.key);
+    
+})
+
+function makesound (key) {
+
+    switch (key) {
         case 'w':
             var tom1 = new Audio('sounds/tom-1.mp3') ;
             tom1.play();
             
             break;
-     
+        
+            case 'a':
+                var tom2 = new Audio('sounds/tom-2.mp3') ;
+                tom2.play();
+                
+                break;
+        
+                case 's':
+                    var tom3 = new Audio('sounds/tom-3.mp3') ;
+                    tom3.play();
+                    
+                    break;
+
+                    case 'd':
+                        var tom4 = new Audio('sounds/tom-4.mp3') ;
+                        tom4.play();
+                        
+                        break;
+        
+                        case 'j':
+                            var snare = new Audio('sounds/snare.mp3') ;
+                            snare.play();
+                            break;
+
+                            case 'k':
+                             var bass = new Audio('sounds/kick-bass.mp3') ;
+                             bass.play();
+                             break ;
+
+                            case 'l':
+                            var crash = new Audio('sounds/crash.mp3') ;
+                            crash.play();
+                            break;
+    
         default:
+            alert("clique errado");
             break;
      }
 
-    })
-   
-}     
+
+}
+
+function animation (currentKey){
+
+var anm = document.querySelector('.' + currentKey);
+
+anm.classList.add('pressed')
+
+setTimeout(function (){
+    anm.classList.remove('pressed')
+},100)
+
+}
--- javascript/objetos.07/index.js
+++ javascript/objetos.07/index.js
@@ -25,3 +25,22 @@ function ManNew(name,age,sex) {
 var man = new ManNew('gustavo',18,'man')
 
 //toda vez q criar um novo objeto dentro da construction function tem q dar um nome de variavel diferente
+
+//criando uma funcao para criar um novo objeto
+
+function ManNew(name,age,sex){
+  this.name = name ;
+  this.age = age ;
+  this.sex = sex ;
+  this.andar = function () {
+      alert('moving ');
+}
+}
+
+//criando um novo objeto
+
+var man = new ManNew('hyro',15,'masc')
+
+//chamando o metodo incluido na function contructor acima ; 
+
+man.andar ()
